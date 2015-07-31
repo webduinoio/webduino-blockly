f2c66ac1eb5ade552f1badc2a0fcb5302049f23d
Ke, Mingze <mingze.ke@gmail.com>
2015-07-31 17:21:31 +0800

    Generate page dynamically: tutorials/led

---
+++ msg/tutorials/led-2/en.js
@@ -0,0 +1,6 @@
+MSG.backIndex = "Webduino Blockly Tutorials";
+MSG.demoTitle = "Blockly Demo Area";
+MSG.catTutorials = "Tutorials";
+
+MSG.subTitle = "Chapter 1-2 : Click the image control LED";
+MSG.demoDescription = "Click on the image below the bulb and the control LED on or off";
---
+++ msg/tutorials/led-4/en.js
@@ -0,0 +1,7 @@
+MSG.backIndex = "Webduino Blockly Tutorials";
+MSG.demoTitle = "Blockly Demo Area";
+MSG.catTutorials = "Tutorials";
+MSG.timerVariable = "timer";
+
+MSG.subTitle = "Chapter 1-4 : Two blinking LED";
+MSG.demoDescription = "Click on the light bulb, let the two LED start blinking and stops blinking";
