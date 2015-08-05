661b0b6501af77ac8bbfeb589dd0dd58a4fd8ae3
oxxo <oxxo.studio@gmail.com>
2015-08-05 18:57:39 +0800

    add pir tutorial 1 and 2

---
+++ msg/tutorials/pir-2/zh-hant.js
@@ -0,0 +1,6 @@
+MSG.backIndex = "Webduino 積木教學";
+MSG.demoTitle = "Blockly 範例測試區";
+MSG.catTutorials = "教學積木";
+
+MSG.subTitle = "課程 6-2：偵測人體紅外線點亮 LED 燈";
+MSG.demoDescription = "偵測到人體紅外線變化時，讓圖片的燈泡發亮，並點亮 LED 燈";
