65249b9e7bb637bdcc55d648030bc435257ce45f
oxxo <oxxo.studio@gmail.com>
2015-08-05 19:48:00 +0800

    add sound tutorial 1 and 2

---
+++ msg/tutorials/sound-1/en.js
@@ -0,0 +1,6 @@
+MSG.backIndex = "Webduino Blockly Tutorials";
+MSG.demoTitle = "Blockly Demo Area";
+MSG.catTutorials = "Tutorials";
+
+MSG.subTitle = "Chapter 6-1 : Sound detect";
+MSG.demoDescription = "If it detects sound, let the bulb light";
---
+++ msg/tutorials/sound-2/zh-hant.js
@@ -0,0 +1,6 @@
+MSG.backIndex = "Webduino 積木教學";
+MSG.demoTitle = "Blockly 範例測試區";
+MSG.catTutorials = "教學積木";
+
+MSG.subTitle = "課程 7-2：偵測聲音點亮 LED 燈";
+MSG.demoDescription = "偵測到聲音，讓圖片的燈泡發亮，並點亮 LED 燈";
