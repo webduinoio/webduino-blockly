c17264977c123ae8e03c150098bd6ba818338542
oxxo <oxxo.studio@gmail.com>
2015-08-06 17:28:29 +0800

    add shock tutorial 1 and 2

---
+++ msg/tutorials/shock-1/en.js
@@ -0,0 +1,7 @@
+MSG.backIndex = "Webduino Blockly Tutorials";
+MSG.demoTitle = "Blockly Demo Area";
+MSG.catTutorials = "Tutorials";
+MSG.timerVariable = "timer";
+
+MSG.subTitle = "Chapter 10-2 : Shock button to add number";
+MSG.demoDescription = "Shock button to make the numbers plus 1.";
---
+++ msg/tutorials/shock-1/zh-hant.js
@@ -0,0 +1,7 @@
+MSG.backIndex = "Webduino 積木教學";
+MSG.demoTitle = "Blockly 範例測試區";
+MSG.catTutorials = "教學積木";
+MSG.timerVariable = "計時器";
+
+MSG.subTitle = "課程 10-1：震動開關增加數字";
+MSG.demoDescription = "每次晃動震動開關時讓數字加 1。";
