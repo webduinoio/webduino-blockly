c8d11ea270c55d5da1133c038936c19acf1a49cb
oxxo <oxxo.studio@gmail.com>
2015-08-11 18:01:30 +0800

    edit header height

---
+++ msg/tutorials/dht-3/en.js
@@ -0,0 +1,5 @@
+MSG.title = "Webduino Blockly Chapter 5-3: Read and write dht data";
+MSG.subTitle = "Chapter 5-3: Read and write dht data";
+MSG.demoDescription = "Use Firebase, read and write dht data";
+MSG.temperature = "temperature: ";
+MSG.humidity = "humidity: ";
---
+++ msg/tutorials/dht-3/zh-hant.js
@@ -0,0 +1,5 @@
+MSG.title = "Webduino Blockly 課程 5-3：記錄與讀取溫濕度數值";
+MSG.subTitle = "課程 5-3：記錄與讀取溫濕度數值";
+MSG.demoDescription = "使用 firebase 作為資料數，儲存溫度與濕度，讀取資料庫繪製圖形";
+MSG.temperature = "溫度：";
+MSG.humidity = "濕度：";
