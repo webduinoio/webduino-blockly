052c236499cfd165e6be42a7b6149ce04fd5e5f4
oxxo <oxxo.studio@gmail.com>
2015-11-06 18:09:06 +0800

    add photocell tutorial 3

---
+++ msg/tutorials/photocell-3/en.js
@@ -0,0 +1,3 @@
+MSG.title = "Webduino Blockly Chapter 12-3 : Convert photocell (variable) value";
+MSG.subTitle = "Chapter 12-3 : Convert photocell (variable) value";
+MSG.demoDescription = "Using numeric conversion blocks, photosensitive (variable) resistor input converted to a fixed range of values.";
---
+++ msg/tutorials/photocell-3/zh-hant.js
@@ -0,0 +1,3 @@
+MSG.title = "Webduino Blockly 課程 12-3：轉換光敏 ( 可變 ) 電阻數值";
+MSG.subTitle = "課程 12-3：轉換光敏 ( 可變 ) 電阻數值";
+MSG.demoDescription = "利用數值轉換積木，將光敏 ( 可變 ) 電阻輸入的數值轉換為固定的區間數值";
