b4ebeab4b8e4ee13259a39681051864dcca2f513
oxxo <oxxo.studio@gmail.com>
2015-11-26 17:45:01 +0800

    edit RFID's block

--- blocks/msg/zh-hant.js
+++ blocks/msg/zh-hant.js
@@ -414,12 +414,12 @@ Blockly.Msg.WEBDUINO_FIREBASE_ATTRONCE = "屬性";
 
 // RFID
 
-Blockly.Msg.WEBDUINO_RFID_ON = "開始偵測";
-Blockly.Msg.WEBDUINO_RFID_UID = "偵測到的代碼";
+Blockly.Msg.WEBDUINO_RFID_ON = "偵測到訊號";
+Blockly.Msg.WEBDUINO_RFID_UID = "所偵測到的代碼";
 Blockly.Msg.WEBDUINO_RFID_UIDIS = "偵測到的代碼為";
 Blockly.Msg.WEBDUINO_RFID_IF = "如果";
 Blockly.Msg.WEBDUINO_RFID_DO = "執行";
-Blockly.Msg.WEBDUINO_RFID_OUT = "離開偵測範圍";
+Blockly.Msg.WEBDUINO_RFID_OUT = "訊號離開";
 Blockly.Msg.WEBDUINO_RFID_OFF = "停止偵測";
 
 /*
