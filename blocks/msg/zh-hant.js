1ec512a34d761665714a289f17a42e779743b5e7
marty <marty.shu@gmail.com>
2016-01-26 12:24:59 +0800

    fix wrong words

--- blocks/msg/zh-hant.js
+++ blocks/msg/zh-hant.js
@@ -249,9 +249,9 @@ Blockly.Msg.WEBDUINO_ULTRASONIC_NEW_TRIG = "超音波傳感器，Trig";
 Blockly.Msg.WEBDUINO_ULTRASONIC_NEW_ECHO = "  Echo";
 Blockly.Msg.WEBDUINO_ULTRASONIC_GET = "擷取距離";
 Blockly.Msg.WEBDUINO_ULTRASONIC_GET_DISTANCE = "擷取距離，每";
-Blockly.Msg.WEBDUINO_ULTRASONIC_GET_TIME = "豪秒 ( 1/1000 秒 ) 擷取一次";
+Blockly.Msg.WEBDUINO_ULTRASONIC_GET_TIME = "毫秒 ( 1/1000 秒 ) 擷取一次";
 Blockly.Msg.WEBDUINO_ULTRASONIC_GET_DO = "執行";
-Blockly.Msg.WEBDUINO_ULTRASONIC_DISTANCE = "所截取的距離 ( 公分 )";
+Blockly.Msg.WEBDUINO_ULTRASONIC_DISTANCE = "所擷取的距離 ( 公分 )";
 Blockly.Msg.WEBDUINO_ULTRASONIC_STOP = "停止擷取距離";
 
 // DHT
