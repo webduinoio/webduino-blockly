cfa4cb578b66193756fe6d7776689a24d35c829b
oxxo <oxxo.studio@gmail.com>
2016-02-01 17:10:27 +0800

    fixed wrong words

--- blocks/msg/zh-hant.js
+++ blocks/msg/zh-hant.js
@@ -258,7 +258,7 @@ Blockly.Msg.WEBDUINO_ULTRASONIC_STOP = "停止擷取距離";
 
 Blockly.Msg.WEBDUINO_DHT_NEW = "溫濕度傳感器，腳位";
 Blockly.Msg.WEBDUINO_DHT_GET = "偵測溫濕度，每";
-Blockly.Msg.WEBDUINO_DHT_GET_TIME = "豪秒 ( 1/1000 秒 ) 擷取一次";
+Blockly.Msg.WEBDUINO_DHT_GET_TIME = "毫秒 ( 1/1000 秒 ) 擷取一次";
 Blockly.Msg.WEBDUINO_DHT_GET_DO = "執行";
 Blockly.Msg.WEBDUINO_DHT_GET_NOW = "所測得目前的";
 Blockly.Msg.WEBDUINO_DHT_GET_T = "溫度 (攝氏)";
