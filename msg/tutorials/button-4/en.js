b19271d3afd8a27ffa347ed1107d0026d27bca1e
osk2 <sc60714@gmail.com>
2016-12-23 17:55:41 +0800

    Done translation of  button-4

--- msg/tutorials/button-4/en.js
+++ msg/tutorials/button-4/en.js
@@ -1,3 +1,9 @@
 MSG.title = "Webduino Blockly Chapter 4-4 : Click button to play game";
 MSG.subTitle = "Chapter 4-4 : Click button to play game";
 MSG.demoDescription = "Run with NPC";
+MSG.gameStart = "START";
+MSG.beTheFirstToReach = "Be the first to reach";
+MSG.goal = "goal";
+MSG.toWinTheGame = "to win the game";
+MSG.NPC = "NPC: ";
+MSG.player = "Player: ";
