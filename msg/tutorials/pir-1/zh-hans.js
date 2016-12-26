b47d8385828b6295212bab779398dd0be5e87778
osk2 <sc60714@gmail.com>
2016-12-26 13:52:56 +0800

    Finish translation of pir

---
+++ msg/tutorials/pir-1/zh-hans.js
@@ -0,0 +1,3 @@
+MSG.title = "Webduino Blockly 课程 6-1：侦测人体红外线";
+MSG.subTitle = "课程 6-1：侦测人体红外线";
+MSG.demoDescription = "侦测到人体红外线变化时，让图片的灯泡发亮";
---
+++ msg/tutorials/pir-2/zh-hans.js
@@ -0,0 +1,3 @@
+MSG.title = "Webduino Blockly 课程 6-2：侦测人体红外线点亮 LED 灯";
+MSG.subTitle = "课程 6-2：侦测人体红外线点亮 LED 灯";
+MSG.demoDescription = "侦测到人体红外线变化时，让图片的灯泡发亮，并点亮 LED 灯";
