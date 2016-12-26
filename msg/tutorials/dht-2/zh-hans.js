5b6345913c86b27237477a95104979989052f669
osk2 <sc60714@gmail.com>
2016-12-26 11:54:23 +0800

    Finish translation of dht

---
+++ msg/tutorials/dht-2/zh-hans.js
@@ -0,0 +1,5 @@
+MSG.title = "Webduino Blockly 课程 5-2：绘制温湿度图表";
+MSG.subTitle = "课程 5-2：绘制温湿度图表";
+MSG.demoDescription = "使用温湿度传感器，利用温度与湿度绘制视觉化图表";
+MSG.temperature = "温度：";
+MSG.humidity = "湿度：";
---
+++ msg/tutorials/dht-3/zh-hans.js
@@ -0,0 +1,5 @@
+MSG.title = "Webduino Blockly 课程 5-3：使用 Firebase 纪录温湿度数值";
+MSG.subTitle = "课程 5-3：使用 Firebase 纪录温湿度数值";
+MSG.demoDescription = "使用 firebase 作为资料库，储存温度与湿度数值。";
+MSG.temperature = "温度：";
+MSG.humidity = "湿度：";
