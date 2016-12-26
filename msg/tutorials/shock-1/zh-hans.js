b1efa2d27229b261468642b68acd208514d5ac3b
osk2 <sc60714@gmail.com>
2016-12-26 14:17:57 +0800

    Finish translation of shock

---
+++ msg/tutorials/shock-1/zh-hans.js
@@ -0,0 +1,3 @@
+MSG.title = "Webduino Blockly 课程 10-1：震动开关增加数字";
+MSG.subTitle = "课程 10-1：震动开关增加数字";
+MSG.demoDescription = "每次晃动震动开关时让数字加 1。";
---
+++ msg/tutorials/shock-2/zh-hans.js
@@ -0,0 +1,3 @@
+MSG.title = "Webduino Blockly 课程 10-2：震动按钮开关改变图片位置";
+MSG.subTitle = "课程 10-2：震动按钮开关改变图片位置";
+MSG.demoDescription = "透过晃动震动开关，让图片的位置改变";
