8ceb1533ab4643f1709b33002f6fa73984d8b1ce
osk2 <sc60714@gmail.com>
2016-12-26 14:24:31 +0800

    Finish translation of sound

---
+++ msg/tutorials/sound-1/zh-hans.js
@@ -0,0 +1,3 @@
+MSG.title = "Webduino Blockly 课程 7-1：侦测声音";
+MSG.subTitle = "课程 7-1：侦测声音";
+MSG.demoDescription = "侦测到声音，就让图片的灯泡发亮";
---
+++ msg/tutorials/sound-2/zh-hans.js
@@ -0,0 +1,3 @@
+MSG.title = "Webduino Blockly 课程 7-2：侦测声音点亮 LED 灯";
+MSG.subTitle = "课程 7-2：侦测声音点亮 LED 灯";
+MSG.demoDescription = "侦测到声音，让图片的灯泡发亮，并点亮 LED 灯";
