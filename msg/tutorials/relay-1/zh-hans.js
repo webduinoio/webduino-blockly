bf01962d455462e933323633f8f95c9d03092970
osk2 <sc60714@gmail.com>
2016-12-26 13:53:28 +0800

    Finish translation of relay

---
+++ msg/tutorials/relay-1/zh-hans.js
@@ -0,0 +1,3 @@
+MSG.title = "Webduino Blockly 课程 11-1：控制继电器";
+MSG.subTitle = "课程 11-1：控制继电器";
+MSG.demoDescription = "控制继电器，并将状态由灯泡图片显示";
---
+++ msg/tutorials/relay-2/zh-hans.js
@@ -0,0 +1,3 @@
+MSG.title = "Webduino Blockly 课程 11-2：点选图片控制继电器";
+MSG.subTitle = "课程 11-2：点选图片控制继电器";
+MSG.demoDescription = "点选下图的灯泡，控制继电器的开或关";
