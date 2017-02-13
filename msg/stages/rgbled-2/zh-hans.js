44c24a1e0ef0b5c5e1ff97f59b7cdff9e632e2fc
shengyuan <karokya@gmail.com>
2017-02-13 14:09:16 +0800

    Make the texts of demo area be the same with blocks.

--- msg/stages/rgbled-2/zh-hans.js
+++ msg/stages/rgbled-2/zh-hans.js
@@ -1,8 +1,8 @@
 MSG.title = "Webduino Blockly 关卡 2：红绿灯控制";
 MSG.subTitle = "关卡 2：红绿灯控制";
 MSG.demoDescription = "点选按钮，切换三色 LED 灯的颜色";
-MSG.red = "红色";
-MSG.green = "绿色";
-MSG.blue = "蓝色";
-MSG.clear = "清除";
+MSG.red = "Red";
+MSG.green = "Green";
+MSG.blue = "Blue";
+MSG.clear = "Clear";
 MSG.stageHelp = "使用积木，点击测试区的按钮时，会改变三色 LED 的颜色, 并且利用模拟器，也表现出灯炮颜色变化的画面。";
--- msg/stages/rgbled-2/zh-hant.js
+++ msg/stages/rgbled-2/zh-hant.js
@@ -1,8 +1,8 @@
 MSG.title = "Webduino Blockly 關卡 2：紅綠燈控制";
 MSG.subTitle = "關卡 2：紅綠燈控制";
 MSG.demoDescription = "點選按鈕，切換三色 LED 燈的顏色";
-MSG.red = "紅色";
-MSG.green = "綠色";
-MSG.blue = "藍色";
-MSG.clear = "清除";
+MSG.red = "Red";
+MSG.green = "Green";
+MSG.blue = "Blue";
+MSG.clear = "Clear";
 MSG.stageHelp = "使用積木，點擊測試區的按鈕時，會改變三色 LED 的顏色, 並且利用模擬器，也表現出燈炮顏色變化的畫面。";
