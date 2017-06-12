a4531d0dcef394c229d31f212b34fec7306c8b2d
shengyuan <karokya@gmail.com>
2017-06-12 14:13:14 +0800

    Add the fifteenth stage

---
+++ msg/stages/max7219-6/zh-hant.js
@@ -0,0 +1,4 @@
+MSG.title = "Webduino Blockly 關卡 15：電子尺";
+MSG.subTitle = "關卡 15：電子尺";
+MSG.demoDescription = "將超音波偵測的距離，顯示在下方及模擬器中的點矩陣。";
+MSG.stageHelp = "使用超音波及點矩陣積木，讓超音波偵測的距離，顯示在範例區，並利用模擬器將超音波偵測的距離顯示在點矩陣上。";
